2ca7e7750798843a69f8e6b5b81e8af9b5c5d191
antc <write@andreasvirkus.me>
2017-05-28 23:09:33 +0300

    Remove unnecessary comments

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,6 @@
 "use strict";
 
-console.log('worker::executing');
-
-/* A version number is useful when updating the worker logic,
-   allowing you to remove outdated cache entries during the update.
-*/
 var version = 'v1::';
-
-/* These resources will be downloaded and cached by the service worker
-   during the installation process. If any resource fails to be downloaded,
-   then the service worker won't be installed either.
-*/
 var offlineFundamentals = [
   '/assets/css/styles.min.css',
   '/assets/js/script.min.js',
@@ -20,30 +10,11 @@ var offlineFundamentals = [
   '/contact/index.html',
 ];
 
-/* The install event fires when the service worker is first installed.
-   You can use this event to prepare the service worker to be able to serve
-   files while visitors are offline.
-*/
 self.addEventListener("install", function(event) {
-  console.log('worker::installing');
-  /* Using event.waitUntil(p) blocks the installation process on the provided
-     promise. If the promise is rejected, the service worker won't be installed.
-  */
   event.waitUntil(
-    /* The caches built-in is a promise-based API that helps you cache responses,
-       as well as finding and deleting them.
-    */
     caches
-      /* You can open a cache by name, and this method returns a promise. We use
-         a versioned cache name here so that we can remove old cache entries in
-         one fell swoop later, when phasing out an older service worker.
-      */
       .open(version + 'fundamentals')
       .then(function(cache) {
-        /* After the cache is opened, we can fill it with the offline fundamentals.
-           The method below will add all resources in `offlineFundamentals` to the
-           cache, after making requests for them.
-        */
         return cache.addAll(offlineFundamentals);
       })
       .then(function() {
@@ -52,11 +23,6 @@ self.addEventListener("install", function(event) {
   );
 });
 
-/* The fetch event fires whenever a page controlled by this service worker requests
-   a resource. This isn't limited to `fetch` or even XMLHttpRequest. Instead, it
-   comprehends even the request for the HTML page on first load, as well as JS and
-   CSS resources, fonts, any images, etc.
-*/
 self.addEventListener("fetch", function(event) {
   console.log('worker::fetching');
 
@@ -70,10 +36,7 @@ self.addEventListener("fetch", function(event) {
     console.log('worker::fetch ignored |', event.request.method, event.request.url);
     return;
   }
-  /* Similar to event.waitUntil in that it blocks the fetch event on a promise.
-     Fulfillment result will be used as the response, and rejection will end in a
-     HTTP response indicating failure.
-  */
+
   event.respondWith(
     caches
       /* This method returns a promise that resolves to a cache entry matching
@@ -91,64 +54,26 @@ self.addEventListener("fetch", function(event) {
            https://ponyfoo.com/articles/progressive-networking-serviceworker
         */
         var networked = fetch(event.request)
-          // We handle the network request with success and failure scenarios.
           .then(fetchedFromNetwork, unableToResolve)
-          // We should catch errors on the fetchedFromNetwork handler as well.
           .catch(unableToResolve);
 
-        /* We return the cached response immediately if there is one, and fall
-           back to waiting on the network as usual.
-        */
-        console.log('worker::fetched', cached ? '(cached)' : '(network)', event.request.url);
         return cached || networked;
 
         function fetchedFromNetwork(response) {
-          /* We copy the response before replying to the network request.
-             This is the response that will be stored on the ServiceWorker cache.
-          */
           var cacheCopy = response.clone();
 
-          console.log('WORKER: fetch response from network.', event.request.url);
-
-          caches
-            // We open a cache to store the response for this request.
-            .open(version + 'pages')
+          caches.open(version + 'pages')
             .then(function add(cache) {
-              /* We store the response for this request. It'll later become
-                 available to caches.match(event.request) calls, when looking
-                 for cached responses.
-              */
               cache.put(event.request, cacheCopy);
             })
             .then(function() {
               console.log('worker::fetch cached |', event.request.url);
             });
 
-          // Return the response so that the promise is settled in fulfillment.
           return response;
         }
 
-        /* When this method is called, it means we were unable to produce a response
-           from either the cache or the network. This is our opportunity to produce
-           a meaningful response even when all else fails. It's the last chance, so
-           you probably want to display a "Service Unavailable" view or a generic
-           error response.
-        */
         function unableToResolve () {
-          /* There's a couple of things we can do here.
-             - Test the Accept header and then return one of the `offlineFundamentals`
-               e.g: `return caches.match('/some/cached/image.png')`
-             - You should also consider the origin. It's easier to decide what
-               "unavailable" means for requests against your origins than for requests
-               against a third party, such as an ad provider.
-             - Generate a Response programmaticaly, as shown below, and return that.
-          */
-
-          console.log('WORKER: fetch request failed in both cache and network.');
-
-          /* Here we're creating a response programmatically. The first parameter is the
-             response body, and the second one defines the options for the response.
-          */
           return new Response('<h1>Service Unavailable</h1>', {
             status: 503,
             statusText: 'Service Unavailable',
@@ -161,36 +86,16 @@ self.addEventListener("fetch", function(event) {
   );
 });
 
-/* The activate event fires after a service worker has been successfully installed.
-   It is most useful when phasing out an older version of a service worker, as at
-   this point you know that the new worker was installed correctly. In this example,
-   we delete old caches that don't match the version in the worker we just finished
-   installing.
-*/
 self.addEventListener("activate", function(event) {
-  /* Just like with the install event, event.waitUntil blocks activate on a promise.
-     Activation will fail unless the promise is fulfilled.
-  */
-  console.log('worker::activating');
-
   event.waitUntil(
-    caches
-      /* This method returns a promise which will resolve to an array of available
-         cache keys.
-      */
-      .keys()
+    caches.keys()
       .then(function (keys) {
-        // We return a promise that settles when all outdated caches are deleted.
         return Promise.all(
           keys
             .filter(function (key) {
-              // Filter by keys that don't start with the latest version prefix.
               return !key.startsWith(version);
             })
             .map(function (key) {
-              /* Return a promise that's fulfilled
-                 when each outdated cache is deleted.
-              */
               return caches.delete(key);
             })
         );
