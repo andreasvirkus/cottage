a2a5e0d43e7973c366fd539d0344c88fe77f04dc
antc <write@andreasvirkus.me>
2018-04-21 19:23:27 +0300

    Add back theme files

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,19 +1,27 @@
+const path = require('path')
+
 module.exports = {
   title: 'ajv',
   description: 'Homepage of Andreas J Virkus',
   evergreen: true,
-  theme: 'ajv',
+  // theme: 'ajv',
   serviceWorker: true,
   head: [
-    ['link', { rel: 'shortcut icon', href: '/favicon.png' }]
-    ['link', { rel: 'apple-touch-icon-precomposed', href: '/favicon.png' }]
-    ['link', { rel: 'alternate', type: 'application/rss+xml', href: '/rss.xml' }]
-    ['meta', { 'http-equiv': 'x-ua-compatible', content: 'ie=edge'}]
-    ['meta', { name: 'theme-color', content: '#222233'}]
-    ['meta', { name: 'msapplication-TileColor', content: '#222233'}]
-    ['meta', { name: 'msapplication-navbutton-color', content: '#222233'}]
+    ['link', { rel: 'shortcut icon', href: '/favicon.png' }],
+    ['link', { rel: 'apple-touch-icon-precomposed', href: '/favicon.png' }],
+    ['link', { rel: 'alternate', type: 'application/rss+xml', href: '/rss.xml' }],
+    ['meta', { 'http-equiv': 'x-ua-compatible', content: 'ie=edge'}],
+    ['meta', { name: 'theme-color', content: '#222233'}],
+    ['meta', { name: 'msapplication-TileColor', content: '#222233'}],
+    ['meta', { name: 'msapplication-navbutton-color', content: '#222233'}],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: '#222233'}]
   ],
+  // configureWebpack: {
+  //   resolve: {
+  //     symlinks: false,
+  //     modules: [path.resolve(__dirname, 'node_modules')]
+  //   }
+  // },
   themeConfig: {
     repo: 'andreasvirkus/vuepress-theme-ajv',
     logo: 'svg/logo.svg',
---
+++ site/.vuepress/theme/Blog.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <h3>{{ description }}</h3>
+
+    <Content/>
+
+    <blog-nav :posts="posts"/>
+  </main>
+</template>
+
+<script>
+import BlogNav from './partials/BlogNav'
+// TODO: Get posts (all .md files from /thoughts dir)
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  components: { BlogNav }
+}
+</script>
+
+<style src="./css/blog.css"></style>
+<style src="./css/blog-nav.css"></style>
