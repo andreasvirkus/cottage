9ca33e87a43a2280cbd057743b7a133e702b1727
ants <andreasvirkus@gmail.com>
2019-10-06 21:19:52 +0300

    Fix 404 page

--- layouts/404.vue
+++ layouts/404.vue
@@ -1,33 +1,24 @@
 <template>
-  <div class="theme-container">
-    <search-box/>
-    <sidebar/>
+  <main class="content">
+    <h1>404</h1>
+    <blockquote>{{ getMsg() }}</blockquote>
 
-    <main>
-      <div class="content">
-        <h1>404</h1>
-        <blockquote>{{ getMsg() }}</blockquote>
+    <p>
+      You've sailed into uncharted territories!
+      I'm not sure why you thought this
+      page would exist, but it seems I didn't
+      have the coding skills to pull it off.
+    </p>
+    <a href="javascript:history.back()">Navigate back</a> or hop to the <saber-link to="/">homepage</saber-link>.
 
-        <p>
-          You've sailed into uncharted territories!
-          I'm not sure why you thought this
-          page would exist, but it seems I didn't
-          have the coding skills to pull it off.
-        </p>
-        <a href="javascript:history.back()">Navigate back</a> or hop to the <saber-link to="/">homepage</saber-link>.
-
-        <p>
-          If you're looking to get in touch, just reach out to me on
-          <a href="https://www.linkedin.com/in/andreasvirkus"
-            title="View my LinkedIn profile"
-            rel="noopener noreferrer">LinkedIn</a> or via
-          <a href="mailto:write@andreasvirkus.me" title="Shoot me a mail!">e-mail</a>.
-        </p>
-      </div>
-    </main>
-
-    <page-footer/>
-  </div>
+    <p>
+      If you're looking to get in touch, just reach out to me on
+      <a href="https://www.linkedin.com/in/andreasvirkus"
+        title="View my LinkedIn profile"
+        rel="noopener noreferrer">LinkedIn</a> or via
+      <a href="mailto:write@andreasvirkus.me" title="Shoot me a mail!">e-mail</a>.
+    </p>
+  </main>
 </template>
 
 <script>
@@ -36,11 +27,10 @@ import SearchBox from '@/components/SearchBox'
 import PageFooter from '@/components/PageFooter'
 
 const msgs = [
-  `Why, it's obvious, Watson... There's nothing here.`,
-  `How did we get here? Is this a Hangover IV in the making?`,
-  `Feelin' mighty adventurous, aren't we?`,
-  `That's your standard Four-Oh-Four right there, cap'n`,
-  `Looks like we've got some broken links. How about you help me fix them?`
+  `Why, it's obvious, Watson... There's nothing here. 🔍`,
+  `Feelin' mighty adventurous, aren't we? 🕺`,
+  `Ahh - standard Four-O-Four right here, cap'n ⛴`,
+  `Looks like we've got some broken links. How about you help me fix them? 🔧`
 ]
 
 export default {
