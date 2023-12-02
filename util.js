23070cf2488effe1472cfe0514e6f8de1d51e39d
andreasvirkus <andreasvirkus@gmail.com>
2023-12-02 02:53:46 +0200

    Make footer time dynamic

--- util.js
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs'
-import format from 'dayjs/plugin/advancedFormat'
-
-dayjs.extend(format)
-
-export function formatPostDate(date, short = false) {
-  const format = short ? 'MMM, Do' : 'DD MMM YYYY'
-  return dayjs(date).format(format)
-}
