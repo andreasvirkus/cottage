733c524ecf5688a1ec56d67bd51ab4b1a1e4fcac
andreasvirkus <andreasvirkus@gmail.com>
2024-12-14 01:01:13 +0200

    chore: tackle unused variables for conic-art

--- src/utils/conic-art.ts
+++ src/utils/conic-art.ts
@@ -17,7 +17,7 @@ const getSeed = (slug: string) => (slug.length * 4) % cutoff
 // Generate a random conic-gradient background based on post slug
 export function getConicGradient(slug: string) {
   // const degree = getRandomInteger(30, 100)
-  const flipDegree = Math.random() > 0.5 ? -1 : 1
+  // const flipDegree = Math.random() > 0.5 ? -1 : 1
   const degree = getRandomDegree(slug)
   const baseColor = colors[slug.length % cutoff]
   let contrastIdx = Math.round(getSeed(slug))
@@ -32,7 +32,7 @@ function getRandomInteger(min: number, max: number) {
   return Math.round(Math.random() * (max - min) + min)
 }
 
-export function getRandomDegree(slug: string) {
+export function getRandomDegree(_slug: string) {
   const flipDegree = Math.random() > 0.5 ? -1 : 1
   return getRandomInteger(1, 4) * 22 * flipDegree + 'deg'
 }
